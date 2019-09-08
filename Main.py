a092aa6974041eae4762466e62237035809b273e
unknown <lovelylayon@gmail.com>
2019-09-08 20:57:44 +0100

    Update Main.py
    * Got layout finished (But needs to be expanded on later)
    * Added method that gets value of the textboxes to allow the user to login (But does not function yet)
    * Added "Log in" button for the user, calls previously mentioned method

--- Main.py
+++ Main.py
@@ -3,11 +3,21 @@ import tkinter
 from tkinter import *
 app = tkinter.Tk()
 app.title("Please login")
+
 # Code to add widgets will go here...
-lblun = Label(app, text="Username", padx=125, pady=5)
-lblun.grid(column=0, row=1)
-txtun = Entry(app, width=10)
-txtun.grid(column=1, row=2)
-app.geometry('350x200')
+label1 = Label(app, text="Username").grid(sticky=E)
+label2 = Label(app, text="Password").grid(sticky=E)
+txtun = Entry(app, width=20)
+txtun.grid(row=0, column=1)
+txtpw = Entry(app, width=20, show="*")
+txtpw.grid(row=1, column=1)
+
+
+def authorizeuser():
+    username = txtun.get()
+    password = txtpw.get()
+
+
+button1 = Button(app, text="Log in", command=authorizeuser).grid(row=2, column=2)
 
 app.mainloop()
