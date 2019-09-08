36fa61a6b54af6c298a3768829882d0bcb62959d
unknown <lovelylayon@gmail.com>
2019-09-08 16:22:06 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python
-
 import tkinter
-top = tkinter.Tk()
+from tkinter import *
+app = tkinter.Tk()
+app.title("Please login")
 # Code to add widgets will go here...
-top.mainloop()
+lblun = Label(app, text="Username", padx=125, pady=5)
+lblun.grid(column=0, row=1)
+txtun = Entry(app, width=10)
+txtun.grid(column=1, row=2)
+app.geometry('350x200')
+
+app.mainloop()
