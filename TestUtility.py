23350f9438d515f7308d00118c5f57376227a14f
unknown <lovelylayon@gmail.com>
2019-09-15 19:25:18 +0100

    Update TestUtility.py
    * Added test method for "roundvalue"
    Test passes successfully

--- TestUtility.py
+++ TestUtility.py
@@ -1,5 +1,12 @@
 import unittest
+from Utility import Utility
 
 
 class MyTestCase(unittest.TestCase):
-
+    def test_roundvalue(self):
+        self.assertEqual(10, Utility.roundvalue(0, 9.51))
+        self.assertEqual(9, Utility.roundvalue(0, 9.49))
+        self.assertEqual(10.5, Utility.roundvalue(1, 10.457))
+        self.assertEqual(10.4, Utility.roundvalue(1, 10.44))
+        self.assertEqual(15.35, Utility.roundvalue(2, 15.354))
+        self.assertEqual(15.34, Utility.roundvalue(2, 15.336))
