bcf7d77f7209676c00570549bb132a6032b6b6e7
unknown <lovelylayon@gmail.com>
2019-09-16 11:15:51 +0100

    Update CalculatorEngine.py
    Added constructor

--- CalculatorEngine.py
+++ CalculatorEngine.py
@@ -1,2 +1,13 @@
 class CalculatorEngine:
 
+    def __init__(self, age_years, weight, height_cm, bmi_category, is_male, exercise_level, height_m, bmr, bmi, rdi):
+        self.age_years = age_years
+        self.weight = weight
+        self.height_cm = height_cm
+        self.bmi_category = bmi_category
+        self.is_male = is_male
+        self.exercise_level = exercise_level
+        self.height_m = height_m
+        self.bmr = bmr
+        self.bmi = bmi
+        self.rdi = rdi
