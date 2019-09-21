e794410293d2c915400e673f4e83437e09917de7
unknown <lovelylayon@gmail.com>
2019-09-21 22:51:33 +0100

    Update Utility.py
    Updated method name for Python Style consistence
    Updated varialbe name for Python Style consistence

--- Utility.py
+++ Utility.py
@@ -1,11 +1,11 @@
 class Utility:
     @staticmethod
-    def roundvalue(decimalplaces, value):
-        if decimalplaces < 0 or decimalplaces > 4:
+    def round_value(decimal_places, value):
+        if decimal_places < 0 or decimal_places > 4:
             return value
         if value <= 0:
             return value
 
-        formattedvalue = round(value, decimalplaces)
+        formattedvalue = round(value, decimal_places)
 
         return formattedvalue
