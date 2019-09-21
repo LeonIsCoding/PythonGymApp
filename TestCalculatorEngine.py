923bd1ff739e62724b5cb1738f41f96500747d59
unknown <lovelylayon@gmail.com>
2019-09-21 19:27:34 +0100

    Update TestCalculatorEngine.py
    Reorganized methods to be consisent with C# solution

--- TestCalculatorEngine.py
+++ TestCalculatorEngine.py
@@ -6,15 +6,6 @@ Engine = CalculatorEngine()
 
 class TestCalculatorEngine(unittest.TestCase):
 
-    def test_calc_bmi(self):
-        Engine.is_male = False
-        Engine.age_years = 21
-        Engine.weight_kg = 64
-        Engine.height_m = 2.1
-        Engine.bmi = Engine.calc_bmi()
-
-        self.assertEqual(14.512471655328797, Engine.get_bmi())
-
     def test_calc_rdi(self):
         self.assertEqual(1200.0, Engine.calc_rdi("Sedentary", 1000))
         self.assertEqual(1375.0, Engine.calc_rdi("Light (1-3 days per week)", 1000))
@@ -31,3 +22,11 @@ class TestCalculatorEngine(unittest.TestCase):
         Engine.set_bmr(Engine.bmr_rhb())
         self.assertEqual(1599.051, Engine.get_bmr())
 
+    def test_calc_bmi(self):
+        Engine.is_male = False
+        Engine.age_years = 21
+        Engine.weight_kg = 64
+        Engine.height_m = 2.1
+        Engine.bmi = Engine.calc_bmi()
+
+        self.assertEqual(14.512471655328797, Engine.get_bmi())
