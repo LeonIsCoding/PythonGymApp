dd72295656756452614a369c4e9ba0afd56961a2
unknown <lovelylayon@gmail.com>
2019-09-21 22:43:59 +0100

    Update TestCalculatorEngine.py
    Set testing method back to original.
    Test now passes successfully.

--- TestCalculatorEngine.py
+++ TestCalculatorEngine.py
@@ -34,5 +34,5 @@ class TestCalculatorEngine(unittest.TestCase):
     def test_calc_bmi_category(self):
         self.assertEqual("Underweight", Engine.calc_bmi_category(16))
         self.assertEqual("Healthy", Engine.calc_bmi_category(19.5))
-        self.assertEqual("Overweight", Engine.calc_bmi_category(24))
+        self.assertEqual("Overweight", Engine.calc_bmi_category(26.4))
         self.assertEqual("Obese", Engine.calc_bmi_category(31))
